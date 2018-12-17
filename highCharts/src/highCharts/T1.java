6da6d7c1fb36301d1f8dc31de98d6283e67a7445
Administrator <Administrator@hongcheng080>
2018-12-17 15:59:28 +0800

    好嗨哟！

--- highCharts/src/highCharts/T1.java
+++ highCharts/src/highCharts/T1.java
@@ -1,7 +1,7 @@
 package highCharts;
 
-public class Test {
+public class T1 {
 	public static void main(String[] args) {
-		System.out.println("helloword£¡£¡");
+		System.out.println("T1");
 	}
 }
---
+++ highCharts/src/highCharts/T2.java
@@ -0,0 +1,8 @@
+package highCharts;
+
+public class T2 {
+	public static void main(String[] args) {
+		System.out.println("T2");
+	}
+
+}
